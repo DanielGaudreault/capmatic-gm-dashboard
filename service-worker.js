30bd752c1a53dd103ec8a4721696dcdafd16eadb
Daniel Gaudreault <163935691+DanielGaudreault@users.noreply.github.com>
2025-06-08 22:33:06 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,13 @@
-const CACHE_NAME = 'capmatic-gm-v1';
-const urlsToCache = ['/', '/index.html', '/css/style.css', '/js/main.js', /* ... other assets ... */];
+const cacheName = 'capmatic-gm-signature-v1';
+const urlsToCache = ['/', '/index.html', '/css/style.css', '/js/main.js', '/css/animation.css', '/js/animation.js'];
 
-self.addEventListener('install', event => {
+self.addEventListener('install", function() {
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-    );
-});
+        caches.open(cacheName).then(cache => cache.addAll(urlsToCache));
+    });
 
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
-});
+    self.addEventListener('fetch', function(event) {
+        event.respondWith(
+        caches.match(event.request').then(response => response || fetch(event.request))
+        );
+    });
